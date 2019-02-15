e865f35b194c4cebc738a25162a42cecf177d049
sande2jm <sande2jm@gmail.com>
2019-02-14 21:48:32 -0500

    j

--- src/App.js
+++ src/App.js
@@ -1,40 +1,28 @@
 import React, { Component } from 'react';
-import axios from 'axios'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-
-  constructor () {
-    super()
-    this.state = {
-      username: ''
-    }
-    this.sendMessageToSQS = this.sendMessageToSQS.bind(this)
-    this.consumeMessage = this.consumeMessage.bind(this)
-  }
-  sendMessageToSQS () {
-    axios.get('http://127.0.0.1:8081/complex/publish_sqs')
-        .then(response => console.log(response))
-  }
-  consumeMessage () {
-    axios.get('http://127.0.0.1:8081/complex/consume_sqs')
-        .then(response => console.log(response))
-  }
-  render () {
+  render() {
     return (
-
-        <div className='button__container'>
-          <button className='button' onClick={this.sendMessageToSQS}>
-            SQS-Publish
-          </button>
-          <button className='button' onClick={this.consumeMessage}>
-            SQS-Consume
-          </button>
-        </div>
-    )
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
   }
 }
-
 
 export default App;
